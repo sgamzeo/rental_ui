f6e28af9480179749e55b01bb15e038b8d47a392
Gamze <89346098+sgamzeo@users.noreply.github.com>
2026-01-04 18:01:24 +0300

    structure

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,14 @@
+abstract class AppRoutes {
+  static const appSelection = '/';
+  static const signIn = '/sign-in';
+  static const signUp = '/sign-up';
+
+  static const navBar = '/nav-bar';
+
+  // bottom nav
+  static const home = '/home';
+  static const explore = '/explore';
+  static const chat = '/chat';
+  static const saved = '/saved';
+  static const profile = '/profile';
+}
